bb29888cd9dbeb6cf2a2e2c83e2fada27cd4783c
Zakariya'u Muhammad <zax20yau@gmail.com>
2018-02-14 09:32:15 +0300

    added the files

---
+++ js/side-nav.js
@@ -0,0 +1,20 @@
+
+
+  // document.addEventListener("DOMContentLoaded", 
+  // 	function(event) {
+  
+
+	function openMenuBtn() {
+		    	document.getElementById("mySidenav")
+		    	.style.width = "300px";
+				}
+
+
+
+	function closeNav() {
+	    document.getElementById("mySidenav").style.width = "0";
+	}
+
+
+
+  // });
